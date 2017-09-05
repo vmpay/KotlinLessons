e4c1b2b600c7bf605befeef685e4880131bbeac5
vmpay <vereszp@gmail.com>
2017-09-05 11:30:22 +0200

    Chapter 11

--- app/src/main/java/eu/vmpay/kotlinlessons/domain/model/DomainClasses.kt
+++ app/src/main/java/eu/vmpay/kotlinlessons/domain/model/DomainClasses.kt
@@ -3,6 +3,11 @@ package eu.vmpay.kotlinlessons.domain.model
 /**
  * Created by andrew on 30.08.17.
  */
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+    val size: Int
+        get() = dailyForecast.size
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
 
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
